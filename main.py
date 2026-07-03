4a300e782f9e38a70c04324f8dbf5adacc5760a6
woosansam <113025977+woosansam@users.noreply.github.com>
2026-07-03 17:31:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import sys
 
+def main(file_path):
+    print(f"Loading data from {file_path}...")
+    df = pd.read_csv(file_path)
+    print("Data successfully loaded!")
+    print(df.head())
+    print(df.describe())
+
+if __name__ == "__main__":
+    main('data/sample_data.csv')
